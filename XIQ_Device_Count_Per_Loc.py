4268627c9ad155da1701a3fc213a3692b64c0fb4
timjsmith24 <20387652+timjsmith24@users.noreply.github.com>
2024-08-22 09:29:11 -0400

    added csv name override flag & credential login backup

--- XIQ_Device_Count_Per_Loc.py
+++ XIQ_Device_Count_Per_Loc.py
@@ -2,6 +2,8 @@
 import json
 import os
 import inspect
+import getpass
+import argparse
 import pandas as pd
 import numpy as np
 import requests
@@ -12,17 +14,21 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 ## Variables 
-XIQ_API_token = '***'
+XIQ_API_token = ''
 
 # count of devices returned per page
 pageSize = 100
 
-csv_file_name = 'Name-of-File.csv'
+csv_file_name = 'Device_Count.csv'
 PATH = current_dir
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', type=str, help="Optional - Overrides the name of the CSV output file") 
+args = parser.parse_args()
 
 URL = "https://api.extremecloudiq.com"
 
-HEADERS = {"Accept": "application/json", "Content-Type": "application/json", 'Authorization':"Bearer " + XIQ_API_token}
+HEADERS = {"Accept": "application/json", "Content-Type": "application/json"}
 
 proxyDict = {
             "http": "",
@@ -79,6 +85,31 @@ def collectDevices():
     page = rawList['page'] + 1 
   return devices
 
+def getAccessToken(XIQ_username, XIQ_password):
+    url = f"{URL}/login"
+    payload = json.dumps({"username": XIQ_username, "password": XIQ_password})
+    response = requests.post(url, headers=HEADERS, data=payload)
+    if response is None:
+      print("ERROR: Not able to login into ExtremeCloudIQ - no response!")
+      print("Script is exiting...")
+      raise SystemExit
+    if response.status_code != 200:
+        print(f"Error getting access token - HTTP Status Code: {str(response.status_code)}")
+        print(f"\t\t{response.text}")
+        print("Script is exiting...")
+        raise SystemExit
+    data = response.json()
+
+    if "access_token" in data:
+        #print("Logged in and Got access token: " + data["access_token"])
+        HEADERS["Authorization"] = "Bearer " + data["access_token"]
+        return 0
+
+    else:
+        print("Unknown Error: Unable to gain access token")
+        print("Script is exiting...")
+        raise SystemExit
+
 # Function to convert floats to ints
 def convert_floats(val):
     if isinstance(val, float):
@@ -86,7 +117,24 @@ def convert_floats(val):
     return val
 
 # MAIN
+
+## XIQ Authorization
+if XIQ_API_token:
+    HEADERS["Authorization"] = "Bearer " + XIQ_API_token
+else:
+    print("Enter your XIQ login credentials")
+    username = input("Email: ")
+    password = getpass.getpass("Password: ")
+    getAccessToken(XIQ_username=username,XIQ_password = password)
+
+# check for csv filename override
+if args.csv:
+   csv_file_name = args.csv
+
+# Collect devices   
 devices = collectDevices()
+
+# build csv data
 csv_data = []
 if devices:
     for device in devices:
@@ -108,9 +156,12 @@ if devices:
             data.update(product_count)
         csv_data.append(data)
 
+# cleanup csv data
 csv_df = pd.DataFrame(csv_data)
 csv_df.fillna(0, inplace=True)
 csv_df = csv_df.apply(lambda col: col.map(convert_floats) if col.dtype == 'float64' else col)
+csv_df['Total'] = csv_df.loc[:, csv_df.columns != 'location'].sum(axis=1)
 
+# save csv data
 print(f"Writing CSV File {csv_file_name}")
 csv_df.to_csv(f"{PATH}/{csv_file_name}", index=False)
--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@ This script will collect the devices from XIQ using API, then build a CSV listin
 ##### Variables
 At the top of the script there are some variables that need to be added.
 1. <span style="color:purple">XIQ_API_token</span> - Update this with a valid token. The token will need the device_list or device:r permission
+    >Note: If XIQ_API_token is blank, the script will prompt for XIQ user credentials
 2. <span style="color:purple">pageSize</span> - This is set to 100 which is the max number of devices that can be returned per API call. This can be lowered if needed.
 3. <span style="color:purple">csv_file_name</span> - this is the name of the file that will be saved.
 4. <span style="color:purple">proxyDict</span> - if a proxy is used you can fill out the http and https to be used.
@@ -22,6 +23,16 @@ open the terminal to the location of the script and run this command.
 
 ```
 python XIQ_Device_Count_Per_Loc.py
+```
+### Flags
+There is an optional flag that can be added to the script when running.
+```
+--csv name_of_file.csv
+```
+This flag will override the csv_file_name variable, allowing you to change the output file name without having to open the script every time.
+You can add the flag when running the script.
+```
+python XIQ_Device_Count_Per_Loc.py --csv New_Device_Count.csv
 ```
 
 ## requirements
